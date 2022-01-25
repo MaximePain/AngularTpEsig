cf04128e7516636c1516589e47a8dd3403c4ebfa
Boulanger Maxime <mmaaxxboumma@gmail.com>
2022-01-25 13:50:29 +0100

    Exo2 fini

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,35 +11,61 @@ export class CalculatorComponent implements OnInit {
 	ngOnInit(): void {}
 
 	textResult = '';
-	lastNbStr = '';
+	firstNb = '';
+	secondNb = '';
 	lastOperande = '';
 	canOverwrite = true;
+	lastWasEqual = false;
 
 	btnClick(char: string) {
 		let nb = 0;
-		if ((nb = parseInt(char)) || nb == 0) {
-			//nombre
+		if ((nb = parseInt(char)) || nb == 0) { //nombre
 			if (this.canOverwrite){
-				this.lastNbStr = this.textResult;
 				this.textResult = '';
 			}
 			this.textResult += char;
 			
 			this.canOverwrite = false;
-		} else {
+		} else if(char != '.') { //operande
+
+			if(this.lastWasEqual && char != "="){
+				this.lastWasEqual = false;
+				this.firstNb = '';
+				this.secondNb = '';
+			}
+
+			if(char == 'x')
+				char = '*';
 			this.canOverwrite = true;
 
-			if(this.lastNbStr != ""){
-				this.textResult = eval(parseInt(this.lastNbStr) + this.lastOperande + parseInt(this.textResult));
+			if(this.firstNb == '')
+				this.firstNb = this.textResult;
+			else if(this.secondNb == '')
+				this.secondNb = this.textResult;
+
+
+			if(this.firstNb != "" && this.secondNb != ""){
+				this.textResult = eval(parseFloat(this.firstNb) + this.lastOperande + parseFloat(this.secondNb));
+				this.firstNb = this.textResult;
+				if(char != "=")
+					this.secondNb = '';
 			}
 
 			if(char != "="){
+				this.textResult += ' ' + char;
 				this.lastOperande = char;
 			}
+			else
+				this.lastWasEqual = true;
+		}
+		else{ //virgule
+			if(!this.textResult.includes('.'))
+				this.textResult += ".";
 		}
 		if(this.textResult.length > 20){
 			this.textResult = "Erreur";
-			this.lastNbStr = '';
+			this.firstNb = '';
+			this.secondNb = '';
 			this.lastOperande = '';
 			this.canOverwrite = true;
 		}
